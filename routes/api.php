36eb4fad93a1887a470af34fed80180c655d4de5
luansantoskr <sluan380@gmail.com>
2021-06-26 14:37:09 -0300

    login adicionado e rotas protegidas

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VacinaController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\RegistroController;
 use Facade\FlareClient\Http\Client;
 
 // Rotas abertas para receber os dados do cliente e vacinas.
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/cliente', [ClienteController::class, 'index']);
 Route::get('/cliente/{id}', [ClienteController::class, 'show']);
 Route::get('/vacinas', [VacinaController::class, 'index']);
@@ -21,6 +24,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/registro_cliente', [RegistroCliente::class, 'store']);
     Route::post('/registro', [RegistroController::class, 'store']);
     Route::post('/vacinas', [VacinaController::class, 'store']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
