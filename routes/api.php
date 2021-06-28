ebacbdb8b2556c0fb1205372d7cc7142137674dc
luansantoskr <sluan380@gmail.com>
2021-06-28 16:44:54 -0300

    atualizando rotas

--- routes/api.php
+++ routes/api.php
@@ -8,30 +8,31 @@ use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\SegundaController;
 use App\Http\Controllers\RegistroController;
 
-// Rotas abertas para receber os dados do cliente e vacinas.
+// Rotas abertas para registro e login.
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+// Rotas privadas de cliente
 Route::get('/cliente', [ClienteController::class, 'index']);
 Route::get('/cliente/{id}', [ClienteController::class, 'show']);
+Route::post('/cliente', [ClienteController::class, 'store']);
+Route::put('/cliente/{id}', [ClienteController::class, 'update']);
+Route::delete('/cliente/{id}', [ClienteController::class, 'destroy']);
+    //Rotas de crud de registro
+Route::get('registro/{id}', [RegistroController::class, 'show']);
+Route::post('/segunda', [SegundaController::class, 'segundaDose']);
+Route::post('/registro', [RegistroController::class, 'store']);
+Route::put('/registro/{id}', [RegistroController::class, 'update']);
+Route::delete('/registro/{id}', [RegistroController::class, 'destroy']);
+    //Rotas de crud de vacina
 Route::get('/vacinas', [VacinaController::class, 'index']);
 Route::get('/vacinas/{id}', [VacinaController::class, 'show']);
-Route::get('registro/{id}', [RegistroController::class, 'show']);
+Route::post('/vacina', [VacinaController::class, 'store']);
+Route::put('/vacina/{id}', [VacinaController::class, 'update']);
+Route::delete('/vacina/{id}', [VacinaController::class, 'destroy']);
+
 
 // Rotas protegidas, necessitam de login.
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    // Rotas privadas de cliente
-    Route::post('/cliente', [ClienteController::class, 'store']);
-    Route::put('/cliente/{id}', [ClienteController::class, 'update']);
-    Route::delete('/cliente/{id}', [ClienteController::class, 'destroy']);
-    //Rotas privadas de registro
-    Route::post('/segunda', [SegundaController::class, 'segundaDose']);
-    Route::post('/registro', [RegistroController::class, 'store']);
-    Route::put('/registro/{id}', [RegistroController::class, 'update']);
-    Route::delete('/registro/{id}', [RegistroController::class, 'destroy']);
-    //Rotas privadas de vacina
-    Route::post('/vacina', [VacinaController::class, 'store']);
-    Route::put('/vacina/{id}', [VacinaController::class, 'update']);
-    Route::delete('/vacina/{id}', [VacinaController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
